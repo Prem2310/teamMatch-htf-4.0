5f64c3c65f1bb6f732a0522e6b70ae237864b2fc
Prem2310 <premrpatel.cse21@aii.ac.in>
2024-02-09 21:03:08 +0530

    sign

--- backend/route/signup.js
+++ backend/route/signup.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/users");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
 const jwt = require("jsonwebtoken");
