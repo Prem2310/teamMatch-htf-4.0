08f5e12bd23a66b1d4907d8c23baf5392d4e2b1e
harshita1611 <harshitabhamhani5@gmail.com>
2024-02-10 20:16:58 +0530

    routing

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,21 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import logo from "../assets/logo.png";
+
 const Navbar = () => {
   return (
     <div className="bg-white">
       <div className="flex justify-between p-3 ">
         <div>
-          {" "}
           <img src={logo} alt="logo" className="h-6" />
         </div>
         <div className="flex">
-          <p className="ml-10 text-lg hover:text-gray-400">Home</p>
-          <p className="ml-10 text-lg hover:text-gray-400">Vision</p>
-          <p className="ml-10 text-lg hover:text-gray-400">About Us</p>
+          <Link to="/" className="ml-10 text-lg hover:text-gray-400">Home</Link>
+          <Link to="/vision" className="ml-10 text-lg hover:text-gray-400">Vision</Link>
+          <Link to="/about" className="ml-10 text-lg hover:text-gray-400">About Us</Link>
         </div>
         <div className="flex">
-          <p className="ml-10 text-lg hover:text-gray-400">Sign Up</p>
-          <p className="ml-10 text-lg hover:text-gray-400">Log In</p>
+          <Link to="/signup" className="ml-10 text-lg hover:text-gray-400">Sign Up</Link>
+          <Link to="/login" className="ml-10 text-lg hover:text-gray-400">Log In</Link>
         </div>
       </div>
       <div>
