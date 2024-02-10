ac032c5a6f1a9aa4210f29e06ae97ffac116946d
harshita1611 <harshitabhamhani5@gmail.com>
2024-02-10 11:33:57 +0530

    login ka bhi frontend kar diya ab neennnddd aa rhi haiii

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,50 +1,63 @@
-// create login page taking email, email and password as input and send data to the server
 import React, { useState } from "react";
-
+import loginPic from "../assets/try3.png";
 
 export default function Login() {
-    const [email, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    
-    async function login() {
-        const response = await fetch("http://localhost:5000/login", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ email, password }),
-        })
-        .then((res) => {
-            return res.json()
-        }
-        ).then((data) => {
-            document.cookie = `LOGIN_INFO=${data.token}`
-            console.log(data)
-        }
-        ).catch((err) => {
-            console.log(err)
-        }
-    )}
-    
-    return (
-        <div className="flex flex-col w-fit justify-center items-center m-auto mt-20">
-        <h1>Login</h1>
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-        <input
+  async function login() {
+    const response = await fetch("http://localhost:5000/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    });
+    const data = await response.json();
+    document.cookie = `LOGIN_INFO=${data.token}`;
+    console.log(data);
+  }
+
+  return (
+    <div className="flex gap-10">
+      <div className="w-2/3 h-full min-h-screen px-7 flex flex-col mt-20">
+        <h1 className="text-5xl font-bold text-black-700 mb-5 tracking-wide italic ">
+          Match Maker OR What
+        </h1>
+        <h1 className="text-5xl font-light text-black-700 mb-5 tracking-wide">
+          Let's find your team
+        </h1>
+        <img
+          src="https://cdn.dribbble.com/users/18463/screenshots/2963559/media/954dd0a6f73d21fb26d13713da763e08.png?resize=800x600&vertical=center"
+          alt="login"
+          className="w-full h-full"
+        />
+      </div>
+      <div className="w-1/2 bg-green-100 flex p-10">
+        <div className="flex flex-col w-full ">
+          <h1 className="text-4xl font-semibold mb-5">LOGIN</h1>
+          <input
             type="text"
-            placeholder="email"
+            placeholder="Email"
             value={email}
-            onChange={(e) => setUsername(e.target.value)}
-            className="border-2 border-black rounded-xl p-2 m-2 w-52"
-        />
-        <input
+            className="border-2 border-black rounded-xl p-2 w-gull mb-5"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
             type="password"
-            placeholder="password"
+            placeholder="Password"
             value={password}
-            className="border-2 border-black rounded-xl p-2 m-2 w-52"
+            className="border-2 border-black rounded-xl p-2 w-full mb-5"
             onChange={(e) => setPassword(e.target.value)}
-        />
-        <button onClick={login} className="bg-blue-400 w-fit p-2 rounded-full">Login</button>
+          />
+          <button
+            onClick={login}
+            className="bg-black text-white p-2 rounded-full w-full mt-5"
+          >
+            Submit
+          </button>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
