b660c0a2e645b7561461dd5716fae54d404ee748
Prem2310 <premrpatel.cse21@aii.ac.in>
2024-02-10 19:47:59 +0530

    abc

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
@@ -6,18 +6,18 @@ const cors = require("cors");
 const app = express();
 const server = http.createServer(app);
 const mongoose = require("mongoose");
-const client=require('./mongodb')
+const client = require("./mongodb");
 const bodyParser = require("body-parser");
 
-mongoose.set('strictQuery',false)
+mongoose.set("strictQuery", false);
 
 const dbURI = process.env.DB_URI;
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.json());
 
 const signup = require("./route/userOperation/signup");
 const login = require("./route/userOperation/login");
@@ -29,20 +29,21 @@ const removeFriend = require("./route/userOperation/removeFriend");
 const getUserById = require("./route/userOperation/getUserById");
 const getLoggedInUser = require("./route/userOperation/getLoggedInUser");
 const createHackathon = require("./route/hackathons/createHackathon");
+const getAllHackathons = require("./route/hackathons/getAllHackathons");
 const getRoomCode = require("./route/chatOperations/getRoomCode");
 
 app.use("/signup", signup);
 app.use("/login", login);
-app.use("/allUsers",allUsers)
-app.use("/addFriend",addFriend)
-app.use("/addSkills",addSkills)
-app.use("/updateUser",updateUser)
-app.use("/removeFriend",removeFriend)
-app.use("/getUserById",getUserById)
-app.use("/getLoggedInUser",getLoggedInUser)
-app.use("/createHackathon",createHackathon)
-app.use("/getRoomCode",getRoomCode)
-
+app.use("/allUsers", allUsers);
+app.use("/addFriend", addFriend);
+app.use("/addSkills", addSkills);
+app.use("/updateUser", updateUser);
+app.use("/removeFriend", removeFriend);
+app.use("/getUserById", getUserById);
+app.use("/getLoggedInUser", getLoggedInUser);
+app.use("/createHackathon", createHackathon);
+app.use("/getAllHackathons", getAllHackathons);
+app.use("/getRoomCode", getRoomCode);
 
 const io = new Server(server, {
   cors: {
@@ -63,25 +64,24 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3000,async () => {
-    console.log("Server is running on port 3000");
+server.listen(3000, async () => {
+  console.log("Server is running on port 3000");
 });
 
-const start=async()=>{
-    try{
-        await mongoose.connect(dbURI)
-        app.listen(5000,()=>{
-            console.log(`Listening on port ${PORT}`);
-        })
-    }
-    catch(err){
-        console.log(err);
-        res.status(500).json({ error: "server side error"})
-    }
-}
+const start = async () => {
+  try {
+    await mongoose.connect(dbURI);
+    app.listen(5000, () => {
+      console.log(`Listening on port ${PORT}`);
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "server side error" });
+  }
+};
 
 app.use("/", (req, res) => {
-    res.send("Hello World");
-  });
+  res.send("Hello World");
+});
 
-start()
+start();
