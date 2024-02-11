993ed6d52e4a5ea1ea10e772aba61398dd9a662f
Devvrat Singh Rathod <rathoddevvratsingh777@gmail.com>
2024-02-11 06:29:57 +0530

    commited

--- frontend/src/components/AddSkills.jsx
+++ frontend/src/components/AddSkills.jsx
@@ -3,8 +3,8 @@ import React, { useState, useEffect } from "react";
 const AddSkills = (props) => {
 
     console.log(props.user, "props.currUser")
-    const [currUser,setCurrUser] = useState({});
-    console.log(currUser, "currUser")
+    // const [currUser,setCurrUser] = useState({});
+    // console.log(currUser, "currUser")
     const [skill, setSkill] = useState("");
     const [skills, setSkills] = useState([]);
 
@@ -40,29 +40,32 @@ const AddSkills = (props) => {
         }
     };
 
-    useEffect(() => {
-        fetch("http://localhost:5000/getLoggedInUser", {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: document.cookie.split("; ").find((row) => row.startsWith("LOGIN_INFO")).split("=")[1],
-            },
-        })
-            .then((res) => {
-                return res.json();
-            })
-            .then((data) => {
-                setCurrUser(data.user);
-                console.log(data);
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-        },[])
+    // useEffect(() => {
+    //     fetch("http://localhost:5000/getLoggedInUser", {
+    //         method: "GET",
+    //         headers: {
+    //             "Content-Type": "application/json",
+    //             Authorization: document.cookie.split("; ").find((row) => row.startsWith("LOGIN_INFO")).split("=")[1],
+    //         },
+    //     })
+    //         .then((res) => {
+    //             return res.json();
+    //         })
+    //         .then((data) => {
+    //             setCurrUser(data.user);
+    //             console.log(data);
+    //         })
+    //         .catch((err) => {
+    //             console.log(err);
+    //         });
+    // },[])
+    
+    const [showcaseSkills, setShowcaseSkills] = useState(false);
 
-
-
-
+    const showSkills = (e) => {
+        e.preventDefault()
+        setShowcaseSkills(!showcaseSkills);
+    }
 
     const removeSkill = (skillToRemove) => {
         setSkills(skills.filter((skill) => skill !== skillToRemove));
@@ -71,21 +74,25 @@ const AddSkills = (props) => {
     return (
         <div>
             <div className="flex flex-wrap gap-2 mb-4">
+
                 {
-            //         props.user && currUser.skills.map((skill, index) => (
-            //        <div
-            //            key={index}
-            //            onClick={() => removeSkill(skill)}
-            //            className="p-2 border-2 border-black rounded-xl w-fit cursor-pointer"
-            //        >
-            //            {skill}
-            //        </div>
-            //    ))
+                    showcaseSkills ? props.user.skills.map((skill, index) => (
+                   <div
+                       key={index}
+                       onClick={() => removeSkill(skill)}
+                       className="p-2 border-2 border-black rounded-xl w-fit cursor-pointer"
+                   >
+                       {skill}
+                   </div>
+               ))
+               :
+               <div></div>
             }
             </div>
             <form className="flex flex-col gap-5">
                 <div className="flex flex-col gap-2">
                     <label htmlFor="skill"> Add Skill</label>
+                    <button onClick={showSkills} className="p-2 border-2 border-black"> Show Skills </button>
                     <input
                         type="text"
                         id="skill"
