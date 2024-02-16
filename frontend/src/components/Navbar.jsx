03d53b7272cc304e60a4e055af8daf1407dfbd6c
Prem2310 <premrpatel.cse21@aii.ac.in>
2024-02-17 01:13:19 +0530

    logo

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,85 +4,102 @@ import { useEffect, useState } from "react";
 
 const Navbar = () => {
   const [currUser, setCurrUser] = useState({});
+  const logoStyle = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "35px", // Adjust the height and width according to your logo size
+    width: "35px",
+    animation: "rotation 1.5s linear infinite", // Adjust animation duration as needed
+  };
+  const keyframes = `
+    @keyframes rotation {
+      from {
+        transform: rotate(0deg);
+      }
+      to {
+        transform: rotate(360deg);
+      }
+    }
+  `;
 
   useEffect(() => {
-    try{
-        const jwt = document.cookie.split("; ").find((row) => row.startsWith("LOGIN_INFO")).split("=")[1];
+    try {
+      const jwt = document.cookie
+        .split("; ")
+        .find((row) => row.startsWith("LOGIN_INFO"))
+        .split("=")[1];
 
-        fetch("https://teammatch-backend.onrender.com/getLoggedInUser", {
+      fetch("https://teammatch-backend.onrender.com/getLoggedInUser", {
         method: "GET",
         headers: {
-            "Content-Type": "application/json",
-            Authorization: document.cookie
+          "Content-Type": "application/json",
+          Authorization: document.cookie
             .split("; ")
             .find((row) => row.startsWith("LOGIN_INFO"))
             .split("=")[1],
         },
-        })
+      })
         .then((res) => {
-            return res.json();
+          return res.json();
         })
         .then((data) => {
-            setCurrUser(data.user);
-            console.log(data);
+          setCurrUser(data.user);
+          console.log(data);
         })
         .catch((err) => {
-            console.log(err);
+          console.log(err);
         });
+    } catch (err) {
+      console.log(err);
     }
-    catch(err){
-        console.log(err);
-    }
-    }, []);
+  }, []);
 
-
-    return (
-        <div className="bg-white font-bricolage">
-            <div className="flex justify-between p-3 ">
-                <div>
-                    <img src={logo} alt="logo" className="h-6" />
-                </div>
-                <div className="flex ">
-                    <a href="/" className="ml-10 text-lg hover:text-gray-400">
-                        Home
-                    </a>
-                    <a href="#vision" className="ml-10 text-lg hover:text-gray-400">
-                        Vision
-                    </a>
-                    
-                    <a href="/searchUser" className="ml-10 text-lg hover:text-gray-400">
-                        Peoples
-                    </a>
-                    <a href="/chats" className="ml-10 text-lg hover:text-gray-400">
-                        Chat Room
-                    </a>
-                </div>
-                <div className="flex">
-                    {
-                        currUser.username &&
-                        <Link to="/profile" className="ml-10 text-lg hover:text-gray-400">
-                        {currUser.username}
-                        </Link>
-                    }
-                    
-                    {
-                        !currUser.username &&
-                        <div>
-
-                            <Link to="/signup" className="ml-10 text-lg hover:text-gray-400">
-                                Sign Up
-                            </Link>
-                            <Link to="/login" className="ml-10 text-lg hover:text-gray-400">
-                                Log In
-                            </Link>
-                        </div>
-                    }
-                </div>
-            </div>
-            <div>
-                <hr className="dark:text-black-900" />
-            </div>
+  return (
+    <div className="bg-white font-bricolage">
+      <div className="flex justify-between p-3 ">
+        <div style={logoStyle}>
+          <style>{keyframes}</style>
+          <img src={logo} alt="Logo" />
         </div>
+        <div className="flex ">
+          <a href="/" className="ml-10 text-lg hover:text-gray-400">
+            Home
+          </a>
+          <a href="#vision" className="ml-10 text-lg hover:text-gray-400">
+            Vision
+          </a>
+
+          <a href="/searchUser" className="ml-10 text-lg hover:text-gray-400">
+            Peoples
+          </a>
+          <a href="/chats" className="ml-10 text-lg hover:text-gray-400">
+            Chat Room
+          </a>
+        </div>
+        <div className="flex">
+          {currUser.username && (
+            <Link to="/profile" className="ml-10 text-lg hover:text-gray-400">
+              {currUser.username}
+            </Link>
+          )}
+
+          {!currUser.username && (
+            <div>
+              <Link to="/signup" className="ml-10 text-lg hover:text-gray-400">
+                Sign Up
+              </Link>
+              <Link to="/login" className="ml-10 text-lg hover:text-gray-400">
+                Log In
+              </Link>
+            </div>
+          )}
+        </div>
+      </div>
+      <div>
+        <hr className="dark:text-black-900" />
+      </div>
+    </div>
   );
 };
 
